f4fdf947541c11cb9c689d1e51e0eb6accf22530
akinmyde <akinremiolumide96@gmail.com>
2023-10-02 16:13:17 +0100

    back to latest

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,13 +9,7 @@ const config: GatsbyConfig = {
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
-    'gatsby-plugin-postcss',
-    {
-      resolve: "gatsby-plugin-antd",
-      options: {
-        style: true,
-      },
-    },
+    'gatsby-plugin-postcss'
   ],
 };
 
